d0c4083e60da3e01625aaa68122ba6a83728188c
Ethereal123 <bsollecito@gmail.com>
2017-02-09 00:38:48 -0500

    finished dlb implementation

--- dlb.java
+++ dlb.java
@@ -1,5 +1,6 @@
+import java.util.*;
+import java.io.*;
 
- 
 public class dlb{
  
 
@@ -13,12 +14,14 @@ public class dlb{
         private Node right;
 		private Node down;
 		private boolean isEnd;
+		float time;
 		
 		private Node(char n,Node r,Node d){
 			c = n;
 			right = r;
 			down = d;
 			isEnd = false;
+			
 		}
     }
 
@@ -40,10 +43,10 @@ public class dlb{
 		Node curr = root;
 		//make a loop that first finds the char in the right going list, then if it finds it it moves down. if we reach the end of the right going list, or the down going list is null at the char
 		//and we arent at the last char reurn false, if we travese all the way o the last char, we find it and the isEnd boolean is true, return true
-		for(int i = 0;i<key.length;i++){
+		for(int i = 0;i<key.length();i++){
 			char c = key.charAt(i);
-			while(curr.right != NULL && c != curr.c){
-				curr = curr.right
+			while(curr.right != null && c != curr.c){
+				curr = curr.right;
 			}
 			if(curr.c == c){
 				curr = curr.down;
@@ -53,7 +56,27 @@ public class dlb{
 			}
 		}
 		return(true); 
-	}		
+	}	
+
+
+	public float gets(String key) {
+		Node curr = root;
+		//make a loop that first finds the char in the right going list, then if it finds it it moves down. if we reach the end of the right going list, or the down going list is null at the char
+		//and we arent at the last char reurn false, if we travese all the way o the last char, we find it and the isEnd boolean is true, return true
+		for(int i = 0;i<key.length();i++){
+			char c = key.charAt(i);
+			while(curr.right != null && c != curr.c){
+				curr = curr.right;
+			}
+			if(curr.c == c){
+				curr = curr.down;
+			}
+			else{
+				return(0);
+			}
+		}
+		return(curr.time); 
+	}
 
     /**
      * Does this symbol table contain the given key?
@@ -63,10 +86,7 @@ public class dlb{
      *     {@code false} otherwise
      * @throws NullPointerException if {@code key} is {@code null}
      */
-    public boolean gets(String key) {
-        return(contains(key));
-    }
-
+  
     /**
      * Inserts the key-value pair into the symbol table, overwriting the old value
      * with the new value if the key is already in the symbol table.
@@ -75,29 +95,32 @@ public class dlb{
      * @param val the value
      * @throws NullPointerException if {@code key} is {@code null}
      */
-    public void put(String key) {
+    public void put(String key, float value) {
        
+		if(root == null){
+			root = new Node(key.charAt(0),null,null);
+		}
 	    Node curr = root;
-	    for(int i = 0;i<key.length;i++){
+	    for(int i = 0;i<key.length();i++){
 	  	    char c = key.charAt(i);
-		    while(curr.right != NULL && curr.c != c){
+		    while(curr.right != null && curr.c != c){
 				curr = curr.right;
 		    }
-		    if(i != key.length-1){
+		    if(i != key.length()-1){
 			    if(curr.c == c){
-				    if(curr.down != NULL){
+				    if(curr.down != null){
 						curr = curr.down;
 				    }
 				    else{
-						curr.down = new Node(key.charAt(i+1),NULL,NULL);
+						curr.down = new Node(key.charAt(i+1),null,null);
 						curr = curr.down;
 					   
 				    }
 			    }
 			    else{
-				    curr.right = new Node(c,NULL,NULL);
+				    curr.right = new Node(c,null,null);
 				    curr = curr.right;
-				    curr.down = new Node(key.charAt(i+1);
+				    curr.down = new Node(key.charAt(i+1),null,null);
 				    curr = curr.down;
 			    }
 			
@@ -106,16 +129,18 @@ public class dlb{
 				//if its the last character then we have to make sure that the boolean is set to true, constructor for the NOde class automatically sets the boolean to false
 				if(curr.c == c){
 					curr.isEnd = true;
+					curr.time = value;
 				}
 				else{
-					curr.right = new Node(c,NULL,NULL);
+					curr.right = new Node(c,null,null);
 					curr = curr.right;
 					curr.isEnd = true;
+					curr.time = value;
 				}
 			}
 		}
 
-  
+	}
 
     /**
      * Returns the number of key-value pairs in this symbol table.
@@ -149,7 +174,28 @@ public class dlb{
      *
      * @param args the command-line arguments
      */
-    public static void main(String[] args) {
+	/*
+    public static void main(String[] args) throws IOException
+	{
+		dlb test = new dlb();
 		
-    }
+		Scanner fscan = null;
+		File dictionary = new File("dictionary.txt");
+		if(dictionary.exists()){
+			fscan = new Scanner(dictionary);
+		}
+		
+		while(fscan.hasNextLine()){
+			String newItem = fscan.nextLine();
+			test.put(newItem,0);
+		}
+		
+		if(test.contains("with")){
+			System.out.println("yay");
+		}
+		
+		if(test.contains("sevbjk;sEkl;jbZDSBK:LJvZXDb")){
+			System.out.println("this wont print");
+		}
+    }*/
 }
